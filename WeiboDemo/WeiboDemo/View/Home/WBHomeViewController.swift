842a3625ef8feb97c77e651c8c8f40a3b6aecfb4
1092592926@qq.com <1092592926@qq.com>
2018-08-27 15:16:46 +0800

    设置主页TableView数据源

--- WeiboDemo/WeiboDemo/View/Home/WBHomeViewController.swift
+++ WeiboDemo/WeiboDemo/View/Home/WBHomeViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
+private let cellId = "cellId"
+
 class WBHomeViewController: WBBaseViewController {
+    
+    private lazy var statusList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,12 +22,37 @@ class WBHomeViewController: WBBaseViewController {
         super.didReceiveMemoryWarning()
     }
     
+    override func loadData() {
+        for i in 0..<10 {
+            statusList.insert(i.description, at: 0)
+        }
+    }
+    
     @objc private func showFriends() {
         
         let vc = WBDemoViewController()
         navigationController?.pushViewController(vc, animated: true)
     }
 }
+
+
+// MARK: - 表格数据源方法
+
+extension WBHomeViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return statusList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel?.text = statusList[indexPath.row]
+        return cell
+    }
+}
+
+
+// MARK: -
 
 extension WBHomeViewController {
     
@@ -33,5 +62,7 @@ extension WBHomeViewController {
 
         navigationItemTitle.leftBarButtonItem = UIBarButtonItem(title: "好友", textColor: UIColor.themeColor, target: self, action: #selector(showFriends))
         
+        // 注册原型cell
+        baseTabeView?.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
 }
