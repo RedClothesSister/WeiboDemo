79ab23e6bae24caef1febef8c90694f89cc2e106
1092592926@qq.com <1092592926@qq.com>
2018-08-29 13:27:42 +0800

    封装网络请求get和post方法

--- WeiboDemo/WeiboDemo/Tools/NetworkService/WBNetworkManager.swift
+++ WeiboDemo/WeiboDemo/Tools/NetworkService/WBNetworkManager.swift
@@ -9,10 +9,39 @@
 import UIKit
 import AFNetworking
 
+// 枚举支持任何的数据类型
+enum WBHTTPMathod {
+    case GET
+    case POST
+}
+
 // 网络管理工具
 class WBNetworkManager: AFHTTPSessionManager {
     
     // 创建单例
     static let shared = WBNetworkManager()
     
+    
+    // 使用一个函数封装AFN的get和post请求
+    func request(method: WBHTTPMathod = .GET, URLString: String, parameters: [String: AnyObject], completion: @escaping (_ json: Any?, _ isSuccess: Bool) -> ()) {
+        
+        // 成功回调
+        let success = { (task: URLSessionDataTask, json: Any?) -> () in
+            completion(json, true)
+        }
+        
+        // 失败回调
+        let failure = { (task: URLSessionDataTask?, error: NSError) -> () in
+            
+            // error通常比较复杂
+            print("网络请求错误", error)
+            completion(nil, false)
+        }
+        
+        if method == .GET {
+            get(URLString, parameters: parameters, progress: nil, success: success, failure: failure as? (URLSessionDataTask?, Error) -> Void)
+        } else {
+            post(URLString, parameters: parameters, progress: nil, success: success, failure: failure as? (URLSessionDataTask?, Error) -> Void)
+        }
+    }
 }
--- WeiboDemo/WeiboDemo/View/Home/WBHomeViewController.swift
+++ WeiboDemo/WeiboDemo/View/Home/WBHomeViewController.swift
@@ -24,6 +24,15 @@ class WBHomeViewController: WBBaseViewController {
     
     override func loadData() {
         
+        // 用AFN网络工具加载微博数据
+        let urlString = "https://api.weibo.com/2/statuses/home_timeline.json"
+        let params = ["access_token": "2.00heeKrFSw4QDB874ca1a2abuDmXuB"]
+        
+        // 网络请求
+        WBNetworkManager.shared.request(method: .GET, URLString: urlString, parameters: params as [String : AnyObject]) { (json, isSuccess) in
+            print(json ?? "")
+        }
+        
         // 模拟 延时 加载数据
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             for i in 0..<20 {
