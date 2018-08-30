e2c8396c6988883c1037bc616cdd2ef1b3c9e8fd
1092592926@qq.com <1092592926@qq.com>
2018-08-30 10:51:43 +0800

    建立微博数据模型

---
+++ WeiboDemo/WeiboDemo/Model/WBStatuses.swift
@@ -0,0 +1,23 @@
+//
+//  WBStatuses.swift
+//  WeiboDemo
+//
+//  Created by 黄君伟 on 2018/8/30.
+//  Copyright © 2018年 黄君伟. All rights reserved.
+//
+
+import UIKit
+import YYModel
+
+// 微博数据模型
+class WBStatuses: NSObject {
+    // 微博ID
+    var id: Int64 = 0
+    // 微博信息内容
+    var text: String?
+    
+    // 重写description 的计算型属性
+    override var description: String {
+        return yy_modelDescription()
+    }
+}
