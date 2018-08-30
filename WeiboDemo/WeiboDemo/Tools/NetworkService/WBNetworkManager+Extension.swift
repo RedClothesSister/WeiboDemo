0b9fab68572981914da4bf726fa9a783953994b9
1092592926@qq.com <1092592926@qq.com>
2018-08-30 10:25:55 +0800

    建立tokenRequest方法，单独处理token

--- WeiboDemo/WeiboDemo/Tools/NetworkService/WBNetworkManager+Extension.swift
+++ WeiboDemo/WeiboDemo/Tools/NetworkService/WBNetworkManager+Extension.swift
@@ -13,10 +13,9 @@ extension WBNetworkManager {
     func statuses(completion: @escaping (_ list: [[String: AnyObject]]?, _ isSuccess: Bool) -> ()) {
         // 用AFN网络工具加载微博数据
         let urlString = "https://api.weibo.com/2/statuses/home_timeline.json"
-        let params = ["access_token": "2.00heeKrFSw4QDB874ca1a2abuDmXuB"]
         
         // 网络请求
-        request(method: .GET, URLString: urlString, parameters: params as [String : AnyObject]) { (json, isSuccess) in
+        tokenRequest(method: .GET, URLString: urlString, parameters: nil) { (json, isSuccess) in
             //print(json)
             // 从json中获取字典数组  如果 as？失败，result为nil
             let result = (json as! NSDictionary)["statuses"] as? [[String: AnyObject]]
--- WeiboDemo/WeiboDemo/Tools/NetworkService/WBNetworkManager.swift
+++ WeiboDemo/WeiboDemo/Tools/NetworkService/WBNetworkManager.swift
@@ -21,9 +21,35 @@ class WBNetworkManager: AFHTTPSessionManager {
     // 创建单例
     static let shared = WBNetworkManager()
     
+    // 访问令牌, 所有的网络请求都基于此令牌(登录除外)
+    var accessToken: String? = "2.00heeKrFSw4QDB874ca1a2abuDmXuB"
+    
+    // 专门负责拼接token的网络请求方法
+    func tokenRequest(method: WBHTTPMathod = .GET, URLString: String, parameters: [String: AnyObject]?, completion: @escaping (_ json: Any?, _ isSuccess: Bool) -> ()) {
+        
+        // 0.处理token字典
+        guard let token = accessToken else {
+            print("没有token！需要登录")
+            completion(nil, false)
+            
+            return
+        }
+        // 1.判断参数字典是否存在，如果为nil，应该新建一个字典
+        var parameters = parameters
+        if parameters == nil {
+            // 实例化一个字典
+            parameters = [String: AnyObject]()
+        }
+        
+        // 2.设置参数字典
+        parameters!["access_token"] = token as AnyObject
+        
+        // 调用request 发起真正的网络请求
+        request(URLString: URLString, parameters: parameters, completion: completion)
+    }
     
     // 使用一个函数封装AFN的get和post请求
-    func request(method: WBHTTPMathod = .GET, URLString: String, parameters: [String: AnyObject], completion: @escaping (_ json: Any?, _ isSuccess: Bool) -> ()) {
+    func request(method: WBHTTPMathod = .GET, URLString: String, parameters: [String: AnyObject]?, completion: @escaping (_ json: Any?, _ isSuccess: Bool) -> ()) {
         
         // 成功回调
         let success = { (task: URLSessionDataTask, json: Any?) -> () in
