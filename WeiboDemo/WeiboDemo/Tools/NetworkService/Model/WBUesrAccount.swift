a0919489fe9528642156ec5c17078dc967dc9320
1092592926@qq.com <1092592926@qq.com>
2018-09-02 10:06:56 +0800

    建立用户账户模型

---
+++ WeiboDemo/WeiboDemo/Tools/NetworkService/Model/WBUesrAccount.swift
@@ -0,0 +1,26 @@
+//
+//  WBUesrAccount.swift
+//  WeiboDemo
+//
+//  Created by 黄君伟 on 2018/9/2.
+//  Copyright © 2018年 黄君伟. All rights reserved.
+//
+
+import UIKit
+
+// 用户账户信息
+class WBUesrAccount: NSObject {
+    
+    // 访问令牌
+    var access_token: String?
+    
+    // 用户代号
+    var uid: String?
+    
+    //过期日期
+    var expires_in: TimeInterval = 0
+    
+    override var description: String {
+        return yy_modelDescription()
+    }
+}
