b3916ab5f3de42acbad60387c1f3896e54cc36bc
RedClothesSister <1092592926@qq.com>
2018-09-04 08:08:53 +0800

    准备欢迎视图和新特性视图

---
+++ WeiboDemo/WeiboDemo/View/Main/NewFeature/WBNewFeatureView.swift
@@ -0,0 +1,23 @@
+//
+//  WBNewFeatureView.swift
+//  WeiboDemo
+//
+//  Created by 黄君伟 on 2018/9/4.
+//  Copyright © 2018年 黄君伟. All rights reserved.
+//
+
+import UIKit
+
+class WBNewFeatureView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.orange
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
---
+++ WeiboDemo/WeiboDemo/View/Main/NewFeature/WBWelcomeView.swift
@@ -0,0 +1,23 @@
+//
+//  WBWelcomeView.swift
+//  WeiboDemo
+//
+//  Created by 黄君伟 on 2018/9/4.
+//  Copyright © 2018年 黄君伟. All rights reserved.
+//
+
+import UIKit
+
+class WBWelcomeView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.blue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
